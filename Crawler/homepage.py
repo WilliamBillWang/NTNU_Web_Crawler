aec9ed898bb075de4fd272dff08d2a3a76ba6a01
WilliamBill <bill939252@gmail.com>
2026-07-20 15:03:24 +0800

    Decoupling and getting the all news

---
+++ Crawler/homepage.py
@@ -0,0 +1,35 @@
+from playwright.sync_api import sync_playwright
+class HomePageCrawler:
+
+    def __init__(self): #initiallize
+        self.url = "https://www.ntnu.edu.tw"
+        # print("build successfully")
+
+    def get_the_latest_news(self):
+        with sync_playwright() as p:
+            browser = p.chromium.launch(headless = False)
+            page = browser.new_page()
+            page.goto(self.url)
+            # title = page.title()
+            # print(page.title())
+
+            # page.wait_for_timeout(5000)
+            # links =  page.locator("a")
+            news =  page.locator(".index-news-list")
+            count = news.count()
+
+            all_news = []
+            for i in range(count): # interate
+
+                item = news.nth(i)
+                title= item.text_content().strip()
+                url = item.locator("a").get_attribute("href")
+
+                pack_news = {
+                    "title" : title,
+                    "url" : url
+                }
+                all_news.append(pack_news)
+
+            browser.close()
+            return all_news
--- main.py
+++ main.py
@@ -1,16 +1,24 @@
-from playwright.sync_api import sync_playwright
 
-with sync_playwright() as p:
-    browser = p.chromium.launch(headless = False)
-    page = browser.new_page()
-    page.goto("https://www.ntnu.edu.tw")
-    print(page.title())
-    # page.wait_for_timeout(5000)
-    # links =  page.locator("a")
-    news =  page.locator(".index-news-list")
-    title = news.first.locator("span").text_content()
-    url = news.first.locator("a").get_attribute("href")
-    print(title,"\n", url)
+from Crawler.homepage import HomePageCrawler
+
+crawler = HomePageCrawler()
+news = crawler.get_the_latest_news()
+for n in news:
+    print(f"Title : {n["title"]}" )
+    print(f"Url : {n["url"]}" )
+    print("-" * 50)
+
+    
+
+    
+        # print(all_news)
+        # print(title)
+        # print(url)
+        # print("-" * 50)
+
+    # title = news.first.locator("span").text_content()
+    # url = news.first.locator("a").get_attribute("href")
+    # print(title,"\n", url)
     # print(links.count())
-    print("新聞數量" , news.count())
-    browser.close()
+    # print("新聞數量" , news.count())
+    
